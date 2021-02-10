b0c2d58a88add734b2bb48f18a279811260a86d0
maxim-aminev <78865717+maxim-aminev@users.noreply.github.com>
2021-02-11 01:14:54 +0500

    #3

--- Lab3.js
+++ Lab3.js
@@ -1,21 +1,15 @@
 "use strict";
 
-let a = prompt("Введите число:", "");
-
-let getDigitsSum = (x) => {
-  let len = x;
-  let k = 0;
-  let sum = 0;
-  x = Number(x);
-  
-  for (let i = 0; i < len; i++) {
-        k = x % 10;
-        sum += k;
-        x = parseInt(x / 10);
+let obj = {
+  method1() {
+    return this;
+  },
+  method2() {
+    return this;
+  },
+  method3() {
+    alert('method3');
   }
+};
 
-  return result;
-}
-
-let sum = getDigitsSum(a);
-console.log(sum);
+obj.method1().method2().method3();
