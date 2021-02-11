af57874660c2499f46fa8990e625c48a73724be8
maxim-aminev <maxim.amineff@yandex.ru>
2021-02-11 23:40:56 +0500

    #1

--- Lab1/script.js
+++ Lab1/script.js
@@ -1,53 +1,91 @@
 "use strict";
 
-let orderList = [
-    {name:"Pringles", description:"Chips", cost:120, currency:"RU", size:0, weight:200, manufacturersFirm:"Kelloggs", dateOfManufacture:"11.02.2021"}, 
-    {name:"Cheetos", description:"Chips", cost:50, currency:"RU", size:0, weight:100, manufacturersFirm:"Frito-Lay", dateOfManufacture:"11.02.2021"},
-    {name:"Lays", description:"Chips", cost:60, currency:"RU", size:0, weight:100, manufacturersFirm:"Frito-Lay", dateOfManufacture:"11.02.2021"}
-];
-
-let container = document.getElementById('orderContainer');
-let table = document.createElement('table');
-for (let index = 0; index < orderList.length; index++) {
-  let td = document.createElement('td');
-  for (let key in orderList[index]) {
-    let tr = document.createElement('tr');
-    tr.id = key;
-    tr.style.height = '30px';
-    tr.textContent = orderList[index][key] + " ";
-    td.append(tr);
-  }
-  table.append(td);
+function BuildTable(table) {
+    table.innerHTML = "";
+    for (let index = 0; index < orderList.length; index++) {
+        let td = document.createElement('td');
+        for (let key in orderList[index]) {
+            let tr = document.createElement('tr');
+            tr.id = key;
+            tr.style.height = '30px';
+            tr.textContent = orderList[index][key] + " ";
+            td.append(tr);
+        }
+        table.append(td);
+    }
 }
 
-let lengthCountObj = countObj(orderList[0]);
-
-let buttontd = document.createElement('td');
-for (let key in orderList[0]) {
+function BuildGetTable(table) {
+    let gettd = document.createElement('td');
+    gettd.id = "gettd";
+    for (let key in orderList[0]) {
+        let tr = document.createElement('tr');
+        tr.style.height = '30px';
+        let input = document.createElement('input');
+        input.id = key;
+        if (key == 'cost' || key == 'size' || key == 'weight') {
+            input.type = "number";
+        }
+        else {
+            input.type = "text";
+        }
+        tr.append(input);
+        gettd.append(tr);
+    }
     let tr = document.createElement('tr');
-    tr.style.height = '30px';
-    let button = document.createElement('button');
-    button.textContent = "Удалить";
-    button.size = 1;
-    button.addEventListener("click", function buttonClick() {
-        for (let index = 0; index < orderList.length; index++) {
-            let tr = document.getElementById(key);
-            tr.remove();
-        }   
-        button.removeEventListener("click", buttonClick);
-        button.remove();
-        tr.remove();
+    let buttonget = document.createElement('button');
+    buttonget.textContent = "Добавить";
+    buttonget.addEventListener("click", function buttonClick() {
+        let td = document.getElementById('gettd');
+        let inputs = td.getElementsByTagName('input');
+        let data = {};
+
+        for (let index = 0; index < inputs.length; index++) {
+            if (inputs[index].value === "") {
+                data[inputs[index].id] = "-";
+            }
+            else {
+                data[inputs[index].id] = inputs[index].value;
+            }
+        }
+        orderList.push(data);
+        gettd.remove();
+        BuildTable(table);
+        BuildGetTable(table);
+        buttonget.removeEventListener("click", buttonClick);
     });
-    tr.append(button);
-    buttontd.append(tr);
+    tr.append(buttonget);
+    gettd.append(tr);
+    table.append(gettd);
 }
-table.append(buttontd);
-container.append(table);
 
 function countObj(obj) {
     var length = 0;
     for (var key in obj) {
         length++;
     }
+    
     return length;
 }
+
+let orderList = [];
+
+let xhr = new XMLHttpRequest();
+let url = new URL('http://45.67.59.109:2001/orders');
+xhr.open('GET', url);
+xhr.timeout = 10000; // в миллисекундах (10 сек)
+xhr.send();
+xhr.onload = () => {
+    if (xhr.status != 200) {
+        alert('Ошибка ${xhr.status}: ${xhr.statusText}');
+    } else {
+        orderList = JSON.parse(xhr.response);
+        let container = document.getElementById('orderContainer');
+        let table = document.createElement('table');
+        BuildTable(table);
+        container.append(table);
+        BuildGetTable(table);
+        container.append(table);
+        let lengthCountObj = countObj(orderList[0]);
+    }
+  };
